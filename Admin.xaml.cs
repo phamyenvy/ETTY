040c3ae5ced8937265aba8d2d9dad6d538211e18
HaQTruong <43584133+HaQTruong@users.noreply.github.com>
2019-06-03 16:47:22 +0700

    Main Admin
    Bổ sung code phục vụ cho các chức năng khác

---
+++ Admin.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LTUDQL2_QUAN_LY_CCVIDEO.Views.Adminstator
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+            VideoGeneral a = new VideoGeneral();
+            content.Children.Clear();
+            content.Children.Add(a);
+        }
+
+        
+
+        private void UploadItem_Selected(object sender, RoutedEventArgs e)
+        {
+            UploadVideo  a = new UploadVideo();
+            content.Children.Clear();
+            content.Children.Add(a);
+
+        }
+
+        private void GeneralItem_Selected(object sender, RoutedEventArgs e)
+        {
+            VideoGeneral a = new VideoGeneral();
+            content.Children.Clear();
+            content.Children.Add(a);
+        }
+
+        private void AccountItem_Selected(object sender, RoutedEventArgs e)
+        {
+            AccountGeneral a = new AccountGeneral();
+            content.Children.Clear();
+            content.Children.Add(a);
+        }
+    }
+}
