cf9bb3f2d8cb3fb959978286e51c057579a46238
HaQTruong <43584133+HaQTruong@users.noreply.github.com>
2019-06-03 16:52:48 +0700

    Upload Video
    Hoàn thành: Upload và copy file đến server(sử dụng System.IO.Copy)!
    Chưa hoàn thành: Thread, chưa có tiến trình để người dùng biết.

--- LTUDQL2_QUAN_LY_CCVIDEO/LTUDQL2_QUAN_LY_CCVIDEO/Views/Adminstator/UploadVideo.xaml.cs
+++ LTUDQL2_QUAN_LY_CCVIDEO/LTUDQL2_QUAN_LY_CCVIDEO/Views/Adminstator/UploadVideo.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,20 +22,57 @@ namespace LTUDQL2_QUAN_LY_CCVIDEO.Views.Adminstator
     /// </summary>
     public partial class UploadVideo : UserControl
     {
+        QuanLyCCVEntities ccv;
+        List<Video> videos;
+        List<LoaiVideo> loaiVideos;
+        List<CapDoTaiKhoan> capDoTaiKhoans;
+        //Ten file poster, trailer, video
+        string sourceImgName;
+        string sourceTrailerName;
+        string sourceVideoName;
         public UploadVideo()
         {
             InitializeComponent();
+
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            ccv = new QuanLyCCVEntities();
+            loaiVideos = ccv.LoaiVideos.ToList();
+            cbCat.ItemsSource = loaiVideos;
+            capDoTaiKhoans = ccv.CapDoTaiKhoans.ToList();
+            cbLevel.ItemsSource = capDoTaiKhoans;
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //   throw new NotImplementedException();
         }
 
         private void btnOpenPoster_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            openFileDialog.Filter = "Image files (*.jpg;*.png;*.jpeg;*.gif;*.bmp)|*.jpg;*.png;*.jpeg;*.gif;*.bmp|All files (*.*)|*.*";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             if (openFileDialog.ShowDialog() == true)
             {
                 lblLinkPoster.Content = openFileDialog.FileName.ToString();
+                sourceImgName = openFileDialog.SafeFileName;
+                // var a = openFileDialog.InitialDirectory;
+                // MessageBox.Show(a);
             }
         }
 
@@ -42,11 +80,13 @@ namespace LTUDQL2_QUAN_LY_CCVIDEO.Views.Adminstator
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Media files (*.mp4)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Filter = "Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV|All files (*.*)|*.*";
+
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             if (openFileDialog.ShowDialog() == true)
             {
-                lblLinkTrailer.Content = openFileDialog.FileName.ToString();
+                lblLinkTrailer.Content = openFileDialog.FileName;
+                sourceTrailerName = openFileDialog.SafeFileName;
             }
         }
 
@@ -54,17 +94,76 @@ namespace LTUDQL2_QUAN_LY_CCVIDEO.Views.Adminstator
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Media files (*.mp4)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Filter = "Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV|All files (*.*)|*.*";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             if (openFileDialog.ShowDialog() == true)
             {
                 lblLinkFilm.Content = openFileDialog.FileName.ToString();
+                sourceVideoName = openFileDialog.SafeFileName;
             }
+        }
+
+        void Refresh()
+        {
+            txtName.Clear();
+            lblLinkFilm.Content = null;
+            lblLinkPoster.Content = null;
+            lblLinkTrailer.Content = null;
+            cbCat.SelectedIndex = 0;
+            txtDes.Clear();
+            cbLevel.SelectedIndex = 0;
+
         }
 
         private void btnUpload_Click(object sender, RoutedEventArgs e)
         {
+            ccv = new QuanLyCCVEntities();
+            Video v = new Video();
+
+            v.MoTa = txtDes.Text; //Mô tả (nếu có)
+            try //Bắt buộc các thuộc tính có trên màn hình đều phải có dữ liệu
+            {
+                v.TenVideo = txtName.Text;
+                v.LinkImage = lblLinkPoster.Content.ToString();
+                v.LinkTrailer = lblLinkTrailer.Content.ToString();
+                v.LinkVideo = lblLinkFilm.Content.ToString();
+                v.NgaySanXuat = DateTime.Parse(dpMFG.SelectedDate.ToString());
+
+                string tenLoai = cbCat.Text;
+                var temp1 = ccv.LoaiVideos.First(p => p.TenLoaiVideo == tenLoai) as LoaiVideo;
+                v.MaLoai_Video = temp1.MaLoaiVideo;
+                string tenCapDo = cbLevel.Text;
+                var temp2 = ccv.CapDoTaiKhoans.First(p => p.TenCapDo == tenCapDo) as CapDoTaiKhoan;
+                v.CapDoVideo = temp2.MaCapDo;
+                v.NgayUpload = DateTime.Now;
+                v.LuotXem = 0;
+                v.Like = 0;
+                v.Dislike = 0;
+                ccv.Videos.Add(v); //add video videoinfo vô database
+
+                try
+                {
+                    string sourceImgPath = v.LinkImage;
+                    string sourceTrailerPath = v.LinkTrailer;
+                    string sourceVideoPath = v.LinkVideo;
+                    string destinationPath = @"D:\LTUDQL2\Trailer\";
+                    System.IO.File.Copy(sourceImgPath, destinationPath + sourceImgName, true);
+                    System.IO.File.Copy(sourceTrailerPath, destinationPath + sourceTrailerName, true);
+                    System.IO.File.Copy(sourceVideoPath, destinationPath + sourceVideoName, true);
+                    Refresh();
+                }
+                catch
+                {
+                    MessageBox.Show("Can't Upload Movie!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Somethings Wrong! Please Check Again!");
+            }
 
         }
+
+
     }
 }
